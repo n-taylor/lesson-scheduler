267456042acee711adb198d361fbafa53c277e8c
n-taylor <natekeithtaylor@gmail.com>
2018-06-05 16:40:00 -0600

    Fix an error in the DeleteAssignmentsTask to allow the project to build

--- app/src/main/java/com/ntaylor/lessonscheduler/tasks/assignment/DeleteClassroomTask.java
+++ app/src/main/java/com/ntaylor/lessonscheduler/tasks/assignment/DeleteClassroomTask.java
@@ -2,5 +2,13 @@ package com.ntaylor.lessonscheduler.tasks.assignment;
 
 import android.os.AsyncTask;
 
-public class DeleteClassroomTask extends AsyncTask {
+public class DeleteClassroomTask extends AsyncTask<Void, Void, Integer> {
+
+
+
+
+    @Override
+    protected Integer doInBackground(Void... voids) {
+        return null;
+    }
 }
