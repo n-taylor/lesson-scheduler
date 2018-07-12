655e785b4c9202212f83148eafaa09d5b0a1e2aa
n-taylor <natekeithtaylor@gmail.com>
2018-07-12 14:42:22 -0600

    Add Assign Activity and prep to add elements to layout

---
+++ app/src/main/java/com/ntaylor/lessonscheduler/AssignFragment.java
@@ -0,0 +1,22 @@
+package com.ntaylor.lessonscheduler;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class AssignFragment extends Fragment {
+
+    public AssignFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_assign, container, false);
+    }
+}
